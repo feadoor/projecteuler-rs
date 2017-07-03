a5cf522d6849d6bf0a69b2cad837837d3ccd98b2
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2017-07-03 13:10:19 +0100

    Add generic depth-first-searcher

--- libs/utils/src/search.rs
+++ libs/utils/src/search.rs
@@ -35,6 +35,68 @@ pub fn binary_search<F>(func: &F, target: u64) -> u64
     lower
 }
 
+/// A structure which is used for iterating through a tree, depth-first, producing only those nodes
+/// which satisfy a particular condition.
+pub struct DepthFirstSearcher<'a, T: 'a> {
+    nodes_to_visit: Vec<T>,
+    children: &'a Fn(&T) -> Vec<T>,
+    should_prune: &'a Fn(&T) -> bool,
+    accept: &'a Fn(&T) -> bool,
+}
+
+impl<'a, T> Iterator for DepthFirstSearcher<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        while let Some(node) = self.nodes_to_visit.pop() {
+            if !(self.should_prune)(&node) {
+                self.nodes_to_visit.append(&mut (self.children)(&node));
+                if (self.accept)(&node) {
+                    return Some(node);
+                }
+            }
+        }
+
+        None
+    }
+}
+
+/// Return a structure for iterating over all nodes in a depth-first structure which satisfy a
+/// particular condition.
+///
+/// Takes an item which is taken to be the root of the tree, a function which produces the children
+/// of a given node, a function `should_prune` which returns `true` if the tree can be pruned above
+/// the given node, and a function `accept` which returns `true` if the current node should be
+/// accepted.
+///
+/// # Examples
+///
+/// ```
+/// use utils::search::depth_first_search;
+///
+/// // Find all numbers below 666 with only odd digits and divisible by 13.
+/// let root = 0;
+/// let children = |x: &u32| [9, 7, 5, 3, 1].iter().map(|d| 10 * x + d).collect();
+/// let should_prune = |x: &u32| *x >= 666;
+/// let accept = |x: &u32| *x > 0 && *x % 13 == 0;
+///
+/// let numbers: Vec<u32> = depth_first_search(root, &children, &should_prune, &accept).collect();
+/// assert_eq!(numbers, vec![117, 13, 195, 351, 377, 39, 533, 559, 91]);
+/// ```
+pub fn depth_first_search<'a, T> (
+    root: T,
+    children: &'a Fn(&T) -> Vec<T>,
+    should_prune: &'a Fn(&T) -> bool,
+    accept: &'a Fn(&T) -> bool
+) -> DepthFirstSearcher<'a, T> {
+    DepthFirstSearcher {
+        nodes_to_visit: vec![root],
+        children: children,
+        should_prune: should_prune,
+        accept: accept,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
