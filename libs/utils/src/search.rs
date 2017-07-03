b15f4cc9187569155b6a59e691bc5749ec622c46
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2017-07-03 13:39:12 +0100

    Move to new trait-based implementation of DFS

--- libs/utils/src/search.rs
+++ libs/utils/src/search.rs
@@ -1,5 +1,7 @@
 //! Implementations of useful utility functions.
 
+use std::marker::Sized;
+
 /// Finds the smallest value of `n` for which func(n) is at least the target value using a binary
 /// search. Assumes that such a value exists, and that the function is increasing.
 ///
@@ -35,65 +37,75 @@ pub fn binary_search<F>(func: &F, target: u64) -> u64
     lower
 }
 
-/// A structure which is used for iterating through a tree, depth-first, producing only those nodes
-/// which satisfy a particular condition.
-pub struct DepthFirstSearcher<'a, T: 'a> {
-    nodes_to_visit: Vec<T>,
-    children: &'a Fn(&T) -> Vec<T>,
-    should_prune: &'a Fn(&T) -> bool,
-    accept: &'a Fn(&T) -> bool,
+/// A trait for nodes in a tree which is to be traversed, depth-first, in the pursuit of nodes which
+/// satisfy a particular condition.
+pub trait DepthFirstNode where Self: Sized {
+    /// Returns all the nodes which are direct descendants of this node.
+    fn children(&self) -> Vec<Self>;
+    /// Returns `true` if the search tree can be pruned above this node - that is, neither it, nor
+    /// any of its children, can possibly satisfy the condition.
+    fn should_prune(&self) -> bool;
+    /// Returns `true` if this node satisfies the condition.
+    fn accept(&self) -> bool;
 }
 
-impl<'a, T> Iterator for DepthFirstSearcher<'a, T> {
+/// A structure which is used for iterating through a tree, depth-first, producing only those nodes
+/// which satisfy a particular condition.
+///
+/// # Examples
+///
+/// ```
+/// use utils::search::{DepthFirstSearcher, DepthFirstNode};
+///
+/// // Find all numbers below 666 with only odd digits and divisible by 13.
+///
+/// struct Node {
+///     value: u32,
+/// }
+///
+/// impl DepthFirstNode for Node {
+///     fn children(&self) -> Vec<Self> {
+///         [9, 7, 5, 3, 1].iter().map(|d| Node { value: 10 * self.value + d }).collect()
+///     }
+///
+///     fn should_prune(&self) -> bool {
+///         self.value >= 666
+///     }
+///
+///     fn accept(&self) -> bool {
+///         self.value > 0 && self.value % 13 == 0
+///     }
+/// }
+///
+/// let root = Node { value: 0 };
+/// let numbers: Vec<u32> = DepthFirstSearcher::new(root).map(|node| node.value).collect();
+/// assert_eq!(numbers, vec![117, 13, 195, 351, 377, 39, 533, 559, 91]);
+/// ```
+pub struct DepthFirstSearcher<T: DepthFirstNode> {
+    nodes_to_visit: Vec<T>,
+}
+
+impl<T: DepthFirstNode> DepthFirstSearcher<T> {
+    /// Construct a new `DepthFirstSearcher` with the given root node.
+    pub fn new(root: T) -> DepthFirstSearcher<T> {
+        DepthFirstSearcher { nodes_to_visit: vec![root] }
+    }
+}
+
+impl<T: DepthFirstNode> Iterator for DepthFirstSearcher<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
         while let Some(node) = self.nodes_to_visit.pop() {
-            if !(self.should_prune)(&node) {
-                self.nodes_to_visit.append(&mut (self.children)(&node));
-                if (self.accept)(&node) {
+            if !node.should_prune() {
+                self.nodes_to_visit.append(&mut node.children());
+                if node.accept() {
                     return Some(node);
                 }
             }
         }
 
         None
-    }
-}
-
-/// Return a structure for iterating over all nodes in a depth-first structure which satisfy a
-/// particular condition.
-///
-/// Takes an item which is taken to be the root of the tree, a function which produces the children
-/// of a given node, a function `should_prune` which returns `true` if the tree can be pruned above
-/// the given node, and a function `accept` which returns `true` if the current node should be
-/// accepted.
-///
-/// # Examples
-///
-/// ```
-/// use utils::search::depth_first_search;
-///
-/// // Find all numbers below 666 with only odd digits and divisible by 13.
-/// let root = 0;
-/// let children = |x: &u32| [9, 7, 5, 3, 1].iter().map(|d| 10 * x + d).collect();
-/// let should_prune = |x: &u32| *x >= 666;
-/// let accept = |x: &u32| *x > 0 && *x % 13 == 0;
-///
-/// let numbers: Vec<u32> = depth_first_search(root, &children, &should_prune, &accept).collect();
-/// assert_eq!(numbers, vec![117, 13, 195, 351, 377, 39, 533, 559, 91]);
-/// ```
-pub fn depth_first_search<'a, T> (
-    root: T,
-    children: &'a Fn(&T) -> Vec<T>,
-    should_prune: &'a Fn(&T) -> bool,
-    accept: &'a Fn(&T) -> bool
-) -> DepthFirstSearcher<'a, T> {
-    DepthFirstSearcher {
-        nodes_to_visit: vec![root],
-        children: children,
-        should_prune: should_prune,
-        accept: accept,
     }
 }
 
