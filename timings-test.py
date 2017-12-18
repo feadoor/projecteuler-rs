f9f94cf4f59e3aab41738b88aff4a6fb1a82d020
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2017-12-18 10:13:36 +0000

    Update the hacky Python script

--- timings-test.py
+++ timings-test.py
@@ -5,9 +5,9 @@ from timeit import timeit
 
 def main():
     if len(sys.argv) < 2:
-        print "Usage: python timings-test.py <path-to-executable>"
+        print "Usage: python timings-test.py <path-to-build-output-dir>"
     else:
-        executable = sys.argv[1]
+        output_dir = sys.argv[1]
         src_files = reduce(lambda x, y: x + y, map(lambda x: x[2], os.walk('src')))
         problem_regex = re.compile("problem(\d{3})\.rs")
         problem_numbers = map(lambda x: int(x.group(1)), filter(lambda x: x is not None, [problem_regex.match(filename) for filename in src_files]))
@@ -15,7 +15,8 @@ def main():
         failed_problems = []
         for problem_number in problem_numbers:
             print "Problem {0:03d}:".format(problem_number),
-            time = timeit(stmt="subprocess.call(['{0}', 'run', '{1}'], stdout=open(os.devnull, 'w'))".format(executable, problem_number), setup="import os, subprocess", number=1)
+            executable = os.path.join(output_dir, "p{0:03d}".format(problem_number))
+            time = timeit(stmt="subprocess.call(['{0}'], stdout=open(os.devnull, 'w'))".format(executable), setup="import os, subprocess", number=1)
             print "{0:.2f} seconds".format(time)
 
             if time > 60:
